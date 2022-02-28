960b2c274c0a30a4fbde8909bf9ac749b222a3fa
hj0503 <857131753@qq.com>
2022-02-28 23:06:52 +0800

    1.两数之和

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -10,8 +10,14 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-
+var twoSum = function (nums, target) {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const x = target - nums[i];
+    if (map.has(x)) {
+      return [map.get(x), i];
+    }
+    map.set(nums[i], i);
+  }
 };
 // @lc code=end
-
