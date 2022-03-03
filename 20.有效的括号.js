724d14b7ab68d3e41aca53d4416f0f69c5a499de
huangj <huangj@getui.com>
2022-03-03 11:31:37 +0800

    first commit

---
+++ 20.有效的括号.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=javascript
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  if (s % 2) return false;
+  let stack = [];
+  for (let item of s) {
+    switch (item) {
+      case '{':
+      case '(':
+      case '[':
+        stack.push(item);
+        break;
+      case '}':
+        if (stack.pop() !== '{') return false;
+        break;
+      case ')':
+        if (stack.pop() !== '(') return false;
+        break;
+      case ']':
+        if (stack.pop() !== '[') return false;
+        break;
+    }
+  }
+  return !stack.length;
+};
+// @lc code=end
