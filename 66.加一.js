4a4786c5d2a8e08883b3af6d1041f67adb648c48
hj0503 <857131753@qq.com>
2022-03-21 23:32:27 +0800

    66.加一

--- 66.加一.js
+++ 66.加一.js
@@ -9,8 +9,17 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-var plusOne = function(digits) {
-
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] === 9) {
+      digits[i] = 0;
+    } else {
+      digits[i] += 1;
+      return digits;
+    }
+  }
+  const ans = new Array(digits.length + 1).fill(0);
+  ans[0] = 1;
+  return ans;
 };
 // @lc code=end
-
