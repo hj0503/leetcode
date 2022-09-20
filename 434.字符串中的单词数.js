54cf3b01388cda0aa3098e4a0e98c440a5323944
hj0503 <857131753@qq.com>
2022-09-20 21:59:55 +0800

    434.字符串中的单词数

--- 434.字符串中的单词数.js
+++ 434.字符串中的单词数.js
@@ -9,8 +9,13 @@
  * @param {string} s
  * @return {number}
  */
-var countSegments = function(s) {
-
+var countSegments = function (s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if ((i === 0 || s[i - 1] === ' ') && s[i] !== ' ') {
+      count++;
+    }
+  }
+  return count;
 };
 // @lc code=end
-
